55157097b9f5657bc9780a5a3d561a90519ddea1
OgumaShotaro <kuyostudy2024@gmail.com>
2025-09-02 11:16:24 +0900

    test

--- 0901.py
+++ 0901.py
@@ -4,7 +4,7 @@ import hashlib
 def main():
     print("main")
     
-    params_n = 32
+    params_n = 3
     
     seed = "cef8256742661ba3b1acb09a2fbca640abf1b072f1db84a888903ba0fff7e909"
     pub_seed = "040daf341a78f98c36a37b3571f3ab26eb80dc782a7273b270d0f8429b572c32"
@@ -12,6 +12,7 @@ def main():
     addr = "85b5abb22a57f390302fc56c94762f852101df4c3c51b7749d49be7a414b6d9e"
 
     seed = bytes.fromhex(seed)
+    
     pub_seed = bytes.fromhex(pub_seed)
     m = bytes.fromhex(m)
     addr = bytes.fromhex(addr)
